39f170d5a41c516815925732a98a2eed7d116ad9
rnoguer22 <rnogugon@myuax.com>
2024-08-04 19:12:51 +0200

    Function to get the model locally

--- read_docs.py
+++ read_docs.py
@@ -15,8 +15,31 @@ import os
 load_dotenv()
 
 model = os.getenv('LLM_MODEL', 'meta-llama/Meta-Llama-3.1-8B-Instruct')
+#Directory to chat with the local docuements in
 rag_directory = os.getenv('DIRECTORY', 'meeting_notes')
 
+
+@st.cache_resource #We cache it with streamlit so that the modal doesnt instantiate when we rerun the code when the UI reoloads
+def get_local_model():
+    """return HuggingFaceEndpoint(
+        repo_id=model,
+        task="text-generation",
+        max_new_tokens=1024,
+        do_sample=False
+    )"""
+
+    #To run the model locally
+    return HuggingFacePipeline.from_model_id(
+        model_id=model,
+        task="text-generation",
+        pipeline_kwargs={
+            "max_new_tokens": 1024,
+            "top_k": 50,
+            "temperature": 0.4
+        }
+    )
+
+llm = get_local_model()
 
 
 
