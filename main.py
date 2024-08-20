fea5a63408127bd1ddcbd5f197586b23a49a4625
rnoguer22 <rnogugon@myuax.com>
2024-08-20 18:17:31 +0200

    Primer commit desde la rama gradio-rag

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import streamlit as st
 from launcher import Launcher
 
 if __name__ == '__main__':
 
-    launcher = Launcher()
-    launcher.launch()
+    def main_page():
+        launcher = Launcher()
+        launcher.launch()
+
+    def page2():
+        st.title("Second page")
+
+    pg = st.navigation([
+        st.Page(main_page, title="Document reader", icon="🔥"),
+        st.Page(page2, title="Gradio page", icon=":material/favorite:"),
+        ])
+    
+    pg.run()
